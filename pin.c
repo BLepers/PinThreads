4f7099b96b0c4ff86f69150d7116a252e54f662f
Baptiste LEPERS <baptiste.lepers@inria.fr>
2013-11-22 14:04:13 +0100

    Correct overloading

--- pin.c
+++ pin.c
@@ -21,8 +21,10 @@ static int next_core = 0;
 
 static void m_init(void);
 static int (*old_pthread_create) (pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
+static int (*old_pthread_setaffinity_np) (pthread_t, size_t, const cpu_set_t *);
+static int (*old_sched_setaffinity) (pid_t, size_t, const cpu_set_t*);
 
-int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg) {
+extern "C" int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg) {
    if(!old_pthread_create)
       m_init();
 
@@ -35,20 +37,20 @@ int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_
 
    CPU_ZERO(&mask);
    CPU_SET(core, &mask);
-   pthread_setaffinity_np(*thread, sizeof(mask), &mask);
+   old_pthread_setaffinity_np(*thread, sizeof(mask), &mask);
 
    printf("-> Set affinity to %d\n", core);
 
    return ret;
 }
 
-int pthread_setaffinity_np(pthread_t thread, size_t cpusetsize, const cpu_set_t *cpuset) {
-   printf("Ignoring call to pthread_setaffinity_np performed by the application\n");
+extern "C" int pthread_setaffinity_np(pthread_t thread, size_t cpusetsize, const cpu_set_t *cpuset) {
+   printf("-> Ignoring call to pthread_setaffinity_np performed by the application\n");
    return 0;
 }
 
-int sched_setaffinity(pid_t pid, size_t cpusetsize, cpu_set_t *mask) {
-   printf("Ignoring call to sched_setaffinity performed by the application\n");
+extern "C" int sched_setaffinity(pid_t pid, size_t cpusetsize, const cpu_set_t *mask) {
+   printf("-> Ignoring call to sched_setaffinity performed by the application\n");
    return 0;
 }
 
@@ -58,7 +60,8 @@ void __attribute__((constructor)) m_init(void) {
    int ncores = get_nprocs();
    char * args;
 
-   int (*old_sched_setaffinity) (pid_t, size_t, cpu_set_t*) = (int (*) (pid_t, size_t, cpu_set_t*)) dlsym(RTLD_NEXT, "sched_setaffinity");
+   old_sched_setaffinity = (int (*) (pid_t, size_t, const cpu_set_t*)) dlsym(RTLD_NEXT, "sched_setaffinity");
+   old_pthread_setaffinity_np = (int (*) (pthread_t, size_t, const cpu_set_t *)) dlsym(RTLD_NEXT, "pthread_setaffinity_np");
    old_pthread_create = (int (*)(pthread_t*, const pthread_attr_t*, void* (*)(void*), void*)) dlsym(RTLD_NEXT, "pthread_create");
 
    cores_len = ncores;
