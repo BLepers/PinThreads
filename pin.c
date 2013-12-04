6115ea0c55d395c82badc647cf9e70301fee3e84
Fabien Gaud <fgaud@sfu.ca>
2013-12-04 04:59:32 -0800

    Properly catch clone too

--- pin.c
+++ pin.c
@@ -2,11 +2,14 @@
 #include "parse_args.h"
 #include "shm.h"
 
+#include <stdarg.h>
+
 static void m_init(void);
 static int (*old_pthread_create) (pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
 static int (*old_pthread_setaffinity_np) (pthread_t, size_t, const cpu_set_t *);
 static int (*old_sched_setaffinity) (pid_t, size_t, const cpu_set_t*);
 static pid_t (*old_fork)(void);
+static int (*old_clone)(int (*)(void *), void *, int, void *, ...);
 
 static int* cores;
 
@@ -55,12 +58,50 @@ int sched_setaffinity(pid_t pid, size_t cpusetsize, const cpu_set_t *mask) {
 
 pid_t fork(void) {
    pid_t ret = old_fork();
-   if(ret == 0) {
-      set_affinity(gettid(), get_next_core(cores));
+   if(ret > 0) {
+      set_affinity(ret, get_next_core(cores));
    }
 
    return ret;
 }
+
+int clone(int (*fn)(void *), void *child_stack, int flags, void *arg, ... ) {
+   va_list arg_list;
+   int ret;
+
+   va_start(arg_list, arg);
+
+   if((flags & CLONE_CHILD_CLEARTID) || (flags & CLONE_CHILD_CLEARTID)) { 
+      pid_t *ptid = va_arg(arg_list, pid_t*);
+      struct user_desc *tls = va_arg(arg_list, struct user_desc*);
+      pid_t *ctid = va_arg(arg_list, pid_t*);
+
+      ret = old_clone(fn, child_stack, flags, arg, ptid, tls, ctid);
+   }
+   else if (flags & CLONE_SETTLS) {
+      pid_t *ptid = va_arg(arg_list, pid_t*);
+      struct user_desc *tls = va_arg(arg_list, struct user_desc*);
+
+      ret = old_clone(fn, child_stack, flags, arg, ptid, tls);
+   }
+   else if(flags & CLONE_PARENT_SETTID) {
+      pid_t *ptid = va_arg(arg_list, pid_t*);
+   
+      ret = old_clone(fn, child_stack, flags, arg, ptid);
+   }
+   else {
+      ret = old_clone(fn, child_stack, flags, arg);
+   }
+
+   va_end(arg_list);
+
+   if(ret > 0) {
+      set_affinity(ret, get_next_core(cores));
+   }
+
+   return ret;
+}
+
 
 void __attribute__((constructor)) m_init(void) {
    if(old_pthread_create) 
@@ -72,6 +113,7 @@ void __attribute__((constructor)) m_init(void) {
    old_pthread_setaffinity_np = (int (*) (pthread_t, size_t, const cpu_set_t *)) dlsym(RTLD_NEXT, "pthread_setaffinity_np");
    old_pthread_create = (int (*)(pthread_t*, const pthread_attr_t*, void* (*)(void*), void*)) dlsym(RTLD_NEXT, "pthread_create");
    old_fork = (pid_t (*)(void)) dlsym(RTLD_NEXT, "fork");
+   old_clone = (int (*)(int (*)(void *), void *, int flags, void *arg, ...)) dlsym(RTLD_NEXT, "clone");
 
    init_shm(getenv("PINTHREADS_SHMID"), 0);
    parse_cores(strdup(getenv("PINTHREADS_CORES")), &cores, NULL);
